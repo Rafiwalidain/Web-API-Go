72f91dc8bdba2721fb6eb40e63807e9360344df7
Muhammad Rafi Walidain <rafiwalidain81@gmail.com>
2026-02-05 20:11:28 +0700

    add initial implementation of main application with routing and handlers

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
 	// This is the main function where the program execution starts.
 
 	router := gin.Default()
+
+	router.GET("/", rootHandler)
+
+	router.GET("/ping", pingHandler)
+
+	router.GET("/book/:id")
+
+	router.Run()
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+		"nama":    "rafi",
+	})
+}
+
+func pingHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "alolo",
+		"nama":    "mamam",
+	})
 }
