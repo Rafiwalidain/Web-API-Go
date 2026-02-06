cacc84191a9601120c2b4d9dbbb917626c61524a
Muhammad Rafi Walidain <rafiwalidain81@gmail.com>
2026-02-06 15:03:36 +0700

    implement book and query handlers with JSON responses

--- main.go
+++ main.go
@@ -15,7 +15,11 @@ func main() {
 
 	router.GET("/ping", pingHandler)
 
-	router.GET("/book/:id")
+	router.GET("/book/:id", booksHandler)
+
+	router.GET("/query", queryHandler)
+
+	router.POST("/book", postBookHandler)
 
 	router.Run()
 }
@@ -32,4 +36,36 @@ func pingHandler(c *gin.Context) {
 		"message": "alolo",
 		"nama":    "mamam",
 	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "book id is " + id,
+	})
+}
+
+func queryHandler(c *gin.Context) {
+	name := c.Query("name")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "query name is " + name,
+	})
+}
+
+type Books struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+func postBookHandler(c *gin.Context) {
+	var newBooks Books
+	if err := c.ShouldBindJSON(&newBooks); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"title":  newBooks.Title,
+			"author": newBooks.Author,
+		})
+	}
 }
