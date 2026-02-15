a912c1525365d199cd6b485ef868ab107ad41ed9
Muhammad Rafi Walidain <rafiwalidain81@gmail.com>
2026-02-15 19:47:43 +0700

    add entity

--- main.go
+++ main.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	// "fmt"
+	"pustaka-api/book"
 	"pustaka-api/handler"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
 	// This is the main function where the program execution starts.
+
+	dsn := "root:root@tcp(127.0.0.1:3306)/pustaka-api-go?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// fmt.Println("Database connected successfully")
+	db.AutoMigrate(&book.Book{})
+
+	book := book.Book{}
+	book.Title = "Belajar Golang ke 3"
+	book.Author = "Rafi walidan"
+	book.Price = 40000
+	book.Rating = 4
+
+	err = db.Create(&book).Error
+	if err != nil {
+		panic("failed to create book")
+	}
 
 	router := gin.Default()
 
@@ -17,11 +41,11 @@ func main() {
 
 	v1.GET("/ping", handler.PingHandler)
 
-	v1.GET("/book/:id", handler.BooksHandler)
+	v1.GET("/books/:id", handler.BooksHandler)
 
 	v1.GET("/query", handler.QueryHandler)
 
-	v1.POST("/book", handler.PostBookHandler)
+	v1.POST("/books", handler.PostBookHandler)
 
 	router.Run()
 }
